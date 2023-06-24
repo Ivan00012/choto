944ff489058c4f60f32c86d4130059850c242b77
Ivan00012 <137327839+Ivan00012@users.noreply.github.com>
2023-06-24 15:00:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import keyboard
 import discord
 import asyncio
-import mouse
-import time
 from discord.ext import commands
+from pypresence import Presence
 from art import tprint
+from config import *
+from time import *
 
 
 async def auto_enchant(sleep_kb, sleep_ms):
@@ -14,76 +14,7 @@ async def auto_enchant(sleep_kb, sleep_ms):
     print(blue_bold + f'auto_enchant(sleep_kb = {sleep_kb}, sleep_ms = {sleep_ms})\n',
           blue_bold + f'Зачаровано кирок - {str(enchanted_pickaxes)}\n',
           blue_bold + f'Потрачено ∼∼ {str(enchanted_pickaxes * 9000)}$\n', sep='')
-    # Авто зачаровывание
-
-    # await asyncio.sleep(3.3)
-    # # sleep_kb = 0.7
-    # # sleep_ms = 0.2
-    # mouse.move(-16, 10, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # mouse.click(button='right')
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('1')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('e')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(16, 0, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # mouse.click(button='right')
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('2')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('e')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(16, 0, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # mouse.click(button='right')
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('3')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('e')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('1')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(-12, 100, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # mouse.click(button='right')
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('9')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(0, -40, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # mouse.click(button='right')
-    # await asyncio.sleep(sleep_ms)
-    # mouse.move(-260, -100, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('3')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(100, 0, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('2')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(80, 10, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # mouse.click(button='left')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(-180, -10, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('3')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('esc')
-    # await asyncio.sleep(sleep_kb)
-    # mouse.move(100, 0, absolute=False, duration=0.1)
-    # await asyncio.sleep(sleep_ms)
-    # kb.press_and_release('3')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('q')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('/')
-    # await asyncio.sleep(sleep_kb)
-    # kb.write('home pve')
-    # await asyncio.sleep(sleep_kb)
-    # kb.press_and_release('enter')
+    p(sleep_kb, sleep_ms)
 
 
 intents = discord.Intents.default()
@@ -105,22 +36,30 @@ magenta = '\033[35;49m'
 async def on_ready():
     global work_channel
     work_channel = bot.get_channel(1121099578947670176)
+    await bot.change_presence(activity=discord.Game(name="WORK"))
     tprint("BOT  WORK !")
 
 
 @bot.command()
 async def start_enchant(ctx):
     if ctx.author.name == 'sam_ivan':
-        await ctx.send('Запускаю цикл')
-        print('Цикл запущен')
         global work_while, enchanted_pickaxes
-        work_while = True
+        if not work_while:
+            await ctx.send('Запускаю цикл')
+            print('Цикл запущен')
+            work_while = True
 
-        while work_while:
-            enchanted_pickaxes += 1
-            await work_channel.send(f'> \n Цикл работает\nЗачаровано кирок-{str(enchanted_pickaxes)}\n')
-            await auto_enchant(0.7, 0.2)
-            await asyncio.sleep(1)
+            while work_while:
+                enchanted_pickaxes += 1
+                await work_channel.send(f'> \n Цикл работает\nЗачаровано кирок-{str(enchanted_pickaxes)}\n')
+                await auto_enchant(0.7, 0.2)
+                await asyncio.sleep(1)
+
+        elif work_while:
+            await ctx.send('Цикл был ранее запущен')
+
+        else:
+            await ctx.send("Как?")
 
     else:
         await ctx.send('Снегур, у тебя недостаточно прав!')
@@ -145,5 +84,24 @@ async def stop_enchant(ctx):
     else:
         await ctx.send('Снегур, у тебя недостаточно прав!')
 
+# RPC = Presence("1121024552655409213")
+#
+# btns = [
+#     {
+#         "label": "Botik",
+#         "url": "https://bot.com"
+#     }
+# ]
+#
+# RPC.connect()
+# RPC.update(
+#     details="qweqwe",
+#     state="123?",
+#     start=time(),
+#     buttons=btns,
+#     large_image='qwer',
+#     small_image='qwer',
+#     large_text="Тут могла бы быть ваша реклама"
+# )
 
-bot.run('myTOKENO')
+bot.run(token)
